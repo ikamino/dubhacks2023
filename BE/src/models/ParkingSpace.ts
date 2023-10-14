4b9b8cb53f0eedb708bbf7aad050149c7fa46a27
ikamino <elee1820@gmail.com>
2023-10-14 15:11:20 -0700

    init models

---
+++ BE/src/models/ParkingSpace.ts
@@ -0,0 +1,9 @@
+interface ParkingSpace {
+    id: string;
+    hostId: string;
+    address: string;
+    pricePerHour: number;
+    isAvailable: boolean;
+    reservedBy?: string;
+    rating: number;
+}
---
+++ BE/src/models/host.ts
@@ -0,0 +1,6 @@
+// Class for a parking space host that can host their own parking space to users.
+class Host {
+    id: number; 
+    userName: string;
+    hostedParkingSpaces: ParkingSpace[];
+}
