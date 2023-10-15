17275f4519ea567dfd6ccd32d3992e42944c0e64
ikamino <elee1820@gmail.com>
2023-10-15 05:29:24 -0700

    listing endpoints

--- BE/src/services/ListingService.ts
+++ BE/src/services/ListingService.ts
@@ -1,4 +1,28 @@
+import axios from "axios";
 export interface ListingService {
-    getListing(listingId: string): Promise<ParkingSpace>;
-    getRating(listingId: string): Promise<Number>;
+    getListings(): Promise<Listing[]>;
+    getListing(listingId: string): Promise<Listing>;
+}
+
+export class Listing implements ListingService {
+    async getListings(): Promise<Listing[]> {
+        try {
+            const listing = await axios.get(`api/routes/listing`);
+            return listing.data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+    async getListing(listingId: string): Promise<Listing> {
+        const listings = await this.getListings();
+        if (listings) {
+            for (const listing of listings) {
+                if (listing.id === listingId) {
+                    return listing;
+                }
+            }
+        }
+        return null;
+    }
 }
